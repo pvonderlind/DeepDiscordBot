e00155529775faf4aed75ec607433af3e69fba76
Philip Vonderlind <philipv@hotmail.de>
2022-10-24 21:10:47 +0200

    Update stable diffusion module to use new syntax and conserve memory.

--- src/stable_diffusion.py
+++ src/stable_diffusion.py
@@ -3,29 +3,25 @@ from torch import autocast
 from diffusers import StableDiffusionPipeline, StableDiffusionImg2ImgPipeline
 from PIL import Image
 
-pipe = StableDiffusionPipeline.from_pretrained(
-    "CompVis/stable-diffusion-v1-4",
-    torch_dtype=torch.float16,
-    custom_pipeline="stable_diffusion_mega",
-    revision="fp16",
-    use_auth_token=True
-)
-pipe.to("cuda")
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", torch_dtype=torch.float16, revision="fp16").to("cuda")
 pipe.enable_attention_slicing()
+# TODO: Replace this hack once change #889 in the repo is in a pip packaged release and components property is released.
+sub_models = {k: v for k, v in vars(pipe).items() if not k.startswith("_")}
+img_pipe = StableDiffusionImg2ImgPipeline(**sub_models)
 # Disable safety checker to avoid (many) false positives
 pipe.safety_checker = lambda images, clip_input: (images, False)
 
 
 def generate_image(prompt: str):
     with autocast("cuda"):
-        image = pipe.text2img(prompt)["sample"][0]
+        image = pipe(prompt).images[0]
     return image
 
 
 def expand_image(image, prompt: str):
     with autocast("cuda"):
         resized_image = resize_image(512, image)
-        generated_img = pipe.img2img(prompt, init_image=resized_image, strength=0.1)["sample"][0]
+        generated_img = img_pipe(prompt, init_image=resized_image, strength=0.7).images[0]
     return generated_img
 
 
